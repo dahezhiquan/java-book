78e800cc254b39b511bb37e5aca5319617ca07f7
dahezhiquan <3390205563@qq.com>
2023-02-06 18:16:44 +0800

    新增Java面向对象编程

---
+++ object/Dog.java
@@ -0,0 +1,28 @@
+package object;
+
+/**
+ * Dog类，面向对象基本
+ */
+public class Dog {
+    // 类的属性
+    String breed;
+    int size;
+    String colour;
+    int age;
+
+    void eat() {
+        System.out.println("我要吃饭！");
+    }
+
+    void run() {
+
+    }
+
+    void sleep() {
+
+    }
+
+    void name() {
+
+    }
+}
